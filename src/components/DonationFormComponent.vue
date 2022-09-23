23d40d78933101f7791052e1789cfb931629af67
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2022-09-23 10:38:14 +0200

    donation email type

--- src/components/DonationFormComponent.vue
+++ src/components/DonationFormComponent.vue
@@ -208,7 +208,7 @@
             />
 
             <input
-                type="text"
+                type="email"
                 name="email"
                 class="bg-transparent focus:bg-white block border my-1 focus:outline-none rounded-none placeholder:text-black/50 px-3 py-1.5 w-full"
                 placeholder="E-mail"
